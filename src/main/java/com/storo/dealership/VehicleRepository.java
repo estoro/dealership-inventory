5c47faea9deb0ab6c6b48783dd509217a4ecbc38
storo <erik.storo@cerner.com>
2019-06-11 09:59:13 -0500

    Fix typo

--- src/main/java/com/storo/dealership/VehicleRepository.java
+++ src/main/java/com/storo/dealership/VehicleRepository.java
@@ -3,6 +3,6 @@ package com.storo.dealership;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface VehicleRepository extends JpaRepository<Vehicle, Long>, QuerydslPredicateExecutor<Vehicle> {
+public interface VehicleRepository extends JpaRepository<Vehicle, String>, QuerydslPredicateExecutor<Vehicle> {
 
 }
